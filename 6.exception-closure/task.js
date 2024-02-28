d789aaa576b0e0ca2a44c0e70b29a7991ba52878
Pavel Chvanov <pchvanov27@gmail.com>
2024-02-28 22:43:02 +0300

    Готово

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,62 @@
-﻿
+﻿function parseCount (z) {
+    let b = Number.parseFloat(z);
+    if(Number.isNaN(b)) {
+        throw new Error("Невалидное значение");
+    }
+    else {
+        return b;
+    }
+}
+
+function validateCount (a) {
+    try {
+        return parseCount(a);
+    }
+    catch (error) {
+        return error;
+    }
+    finally {
+    }
+}
+
+class Triangle {
+    constructor(a, b, c) {
+        if (((a + b) <= c) || ((a + c) <= b) || ((b + c) <= a)) {
+            throw new Error("Треугольник с такими сторонами не существует");
+        }
+        else {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        }
+    }
+    get perimeter() {
+        let p = (this.a + this.b + this.c);
+        return p;
+    }
+    get area () {
+        let p = (this.a + this.b + this.c) / 2;
+        let ar = Math.round(1000 * Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c)) + Number.EPSILON) / 1000;
+        return ar;
+    }
+}
+
+function getTriangle (a, b, c) {
+    try {
+        let trg = new Triangle(a, b, c);
+        return trg;
+    }
+    catch (error) {
+        let err = {
+            get perimeter() {
+               return "Ошибка! Треугольник не существует";
+            },
+            get area() {
+               return "Ошибка! Треугольник не существует";
+            }
+        }
+        return err;
+    }
+    finally {
+    }
+}
